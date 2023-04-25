a6e8591758b75374c02779ad3cf1fc8cf1251e4b
pipecraft <pipecraft.net@gmail.com>
2023-04-26 00:06:52 +0800

    0.0.2

--- build/userscript-prod/miniflux-extension.user.js
+++ build/userscript-prod/miniflux-extension.user.js
@@ -4,17 +4,23 @@
 // @namespace            https://github.com/utags/miniflux-extension
 // @homepage             https://github.com/utags/miniflux-extension#readme
 // @supportURL           https://github.com/utags/miniflux-extension/issues
-// @version              0.0.1
+// @version              0.0.2
 // @description          An extension for Miniflux RSS reader
 // @description:zh-CN    Miniflux RSS 阅读器扩展
 // @icon                 https://miniflux.app/favicon.ico
 // @author               Pipecraft
 // @license              MIT
+// @match                https://reader.miniflux.app/*
 // @match                https://miniflux.pipecraft.net/*
 // @grant                GM_addElement
 // @grant                GM_addStyle
 // ==/UserScript==
 //
+//// Recent Updates
+//// - 0.0.2 2023.04.26
+////    - Add reader.miniflux.app as match site
+////    - Automatically update categories every 10 minutes
+////
 ;(() => {
   "use strict"
   var doc = document
@@ -97,17 +103,23 @@
   }
   var addStyle = (styleText) => GM_addStyle(styleText)
   var content_default =
-    ".miniflux_ext #left_section{position:fixed;top:0;left:0;padding:20px;overflow:auto;height:100%}"
+    "[data-miniflux_ext] #left_section{position:fixed;top:0;left:0;padding:20px;overflow:auto;height:100%}"
   var config = {
-    matches: ["https://miniflux.pipecraft.net/*"],
+    matches: [
+      "https://reader.miniflux.app/*",
+      "https://miniflux.pipecraft.net/*",
+    ],
   }
+  var m1 = 1e3 * 60
   var storageKey = "extension.miniflux.cache"
   function getCategoriesFromCache() {
-    const categoriesText = localStorage.getItem(storageKey) || ""
-    return createElement("div", {
-      class: "items",
-      innerHTML: categoriesText,
-    })
+    const categoriesText = localStorage.getItem(storageKey)
+    return categoriesText
+      ? createElement("div", {
+          class: "items",
+          innerHTML: categoriesText,
+        })
+      : null
   }
   async function updateCategories() {
     const response = await fetch("/categories")
@@ -116,8 +128,8 @@
       const div = createElement("div")
       div.innerHTML = text
       const categories = $(div, ".items")
-      if (categories) {
-        const categoriesText = localStorage.getItem(storageKey) || ""
+      if (categories == null ? void 0 : categories.innerHTML) {
+        const categoriesText = localStorage.getItem(storageKey)
         if (categoriesText !== categories.innerHTML) {
           localStorage.setItem(storageKey, categories.innerHTML)
           return true
@@ -126,30 +138,40 @@
     }
     return false
   }
+  var appendCategories = () => {
+    const container = $("[data-miniflux_ext] #left_section")
+    if (!container) {
+      return
+    }
+    let categories = $("[data-miniflux_ext] #left_section .items")
+    if (categories) {
+      categories.remove()
+    }
+    categories = getCategoriesFromCache()
+    if (categories) {
+      container.append(categories)
+    }
+  }
+  async function autoUpdateCategories() {
+    if (await updateCategories()) {
+      appendCategories()
+      setTimeout(autoUpdateCategories, 3 * m1)
+    } else {
+      setTimeout(autoUpdateCategories, 10 * m1)
+    }
+  }
   async function main() {
-    if (!document.body || $(".miniflux_ext #left_section")) {
+    if (!document.body || $("[data-miniflux_ext] #left_section")) {
       return
     }
     addStyle(content_default)
-    document.body.classList.add("miniflux_ext")
-    const left = addElement(document.body, "section", {
+    document.body.dataset.miniflux_ext = "1"
+    addElement(document.body, "section", {
       id: "left_section",
       innerHTML: `<section class="page-header"><h1>Categories</h1></section>`,
     })
-    let categories = getCategoriesFromCache()
-    if (categories) {
-      left.append(categories)
-    }
-    setTimeout(async () => {
-      if (await updateCategories()) {
-        console.log("updated")
-        categories.remove()
-        categories = getCategoriesFromCache()
-        if (categories) {
-          left.append(categories)
-        }
-      }
-    }, 1e3)
+    appendCategories()
+    setTimeout(autoUpdateCategories, 1e3)
   }
   main()
   addEventListener(document, "DOMContentLoaded", main)
